05b237b9a3a430c8610ee0cd16d800ea43e9ac5e
Anjan Kumar <41472950+anjan111@users.noreply.github.com>
2019-11-27 07:18:55 +0530

    Add files via upload

---
+++ 001_script.py
@@ -0,0 +1,11 @@
+### script mode  / batch mode
+'''
+==>>> create a new file
+==>>> write all python statement
+==>>> save as . py extension
+==>>>  run directly --->> then we can get output on python shell
+'''
+print "hello" # prining hello on screen
+print "hai"   # printinng hai on screen
+a  =123       # storing 123 in memory with name of a 
+print a       # printing data in a variable
---
+++ 002_keywords.py
@@ -0,0 +1,17 @@
+# keywords
+'''
+===>>> Built -in words
+===>>> which are already defined inside python
+===>>> each and every it having its own specific task
+======>> print  , while , if , else  ====>> 31
+
+=====>> keyword python  ---->> we can access by import keyword
+'''
+import keyword # accessing the keyword file
+print keyword.kwlist # showning all keywords
+print len(keyword.kwlist) # showing the no of keywords
+
+
+# next session we continue the following topics
+
+# identifiers , variables , datatypes , built-in functions
